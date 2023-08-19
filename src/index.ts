0f541e0ef8de00ca1d19d0f5ac76cec2e538e2da
Divya Vaibhav <112477955+vkamsingh@users.noreply.github.com>
2023-08-20 00:35:13 +0530

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Ferralink } from './lib/Ferralink';
+import { Player } from './lib/module/Player';
+import Queue from './lib/module/Queue';
+
+export {
+    Ferralink as FerraLink,
+    Player,
+    Queue
+};
