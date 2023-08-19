3ff68725bd30eb8bb4999bfd9da3e256881ccc36
Divya Vaibhav <112477955+vkamsingh@users.noreply.github.com>
2023-08-19 18:53:09 +0000

    change

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Ferralink } from './lib/Ferralink';
-import { Player } from './lib/module/Player';
-import Queue from './lib/module/Queue';
-
-export {
-    Ferralink as FerraLink,
-    Player,
-    Queue
-};
